7c4178acd6bb7524761aace5bd1a441fd91cf8d5
Brandon Trieu <nhatterball123@gmail.com>
2022-07-20 11:55:10 -0400

    content edit

--- ssg/content.py
+++ ssg/content.py
@@ -3,16 +3,17 @@ from yaml import load
 from yaml import FullLoader
 from collections.abc import Mapping
 
-class content(Mapping):
+class Content(Mapping):
     __delimiter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
+    @classmethod
     def load(cls, string):
-        _ = __regex.split(string, 2)
-        fm = __regex.split(string, 2)
-        content = __regex.split(string, 2)
+        _ = cls.__regex.split(string, 2)
+        fm = cls.__regex.split(string, 2)
+        content = cls.__regex.split(string, 2)
 
-        load(fm, Loader = FullLoader)
+        metadata = load(fm, Loader = FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
@@ -20,29 +21,34 @@ class content(Mapping):
         self.data = {"content" : content}
 
     @property
-    def body():
+    def body(self):
         return self.data["content"]
 
     @property
-    def type():
-        if self.data["type"] is None:
-            return None
-        else:
+    def type(self):
+        if "type" in self.data["type"]:
             return self.data["type"]
+        else:
+            return None
     
-    @type.setter(self.data["type"])
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
 
     def __iter__(self):
-        self.data()
+        self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
 
     def __repr__(self):
         data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
         return str(data)
 
 
