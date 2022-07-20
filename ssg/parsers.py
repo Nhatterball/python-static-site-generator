2964cf17766b09bba3d513620d3c3d88b7451234
Brandon Trieu <nhatterball123@gmail.com>
2022-07-20 14:40:26 -0400

    subclass

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,10 @@
 from typing import List
 from pathlib import Path
 import shutil
+import sys 
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 class Parser:
     extensions : List[str] = []
@@ -28,3 +32,24 @@ class ResourceParser(Parser):
 
     def parse(self,path,source,dest):
         self.copy(path, source, dest)
+
+class MarkdownParser:
+    extension = [".md", ",markdown"]
+
+    def parse(self, path: Path, source : Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = self.markdown(content.body)
+        self.write(html, path, dest)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        raise NotImplementedError
+
+class ReStructuredTextParser:
+    extensions = [",rst"]
+    def parse(self, path: Path, source : Path, dest: Path, writer_name: "html5"):
+        content = Content.load(self.read(path))
+        self.publish_parts(content.body)
+        self.write(html["html_body"], path, dest)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        raise NotImplementedError
+
+    
