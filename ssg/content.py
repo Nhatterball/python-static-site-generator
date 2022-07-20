f26ba99e91a0e5cb924b08abe8594d8f6250c3ba
Brandon Trieu <nhatterball123@gmail.com>
2022-07-20 12:00:43 -0400

    sdf

--- ssg/content.py
+++ ssg/content.py
@@ -26,11 +26,8 @@ class Content(Mapping):
 
     @property
     def type(self):
-        if "type" in self.data["type"]:
-            return self.data["type"]
-        else:
-            None
-    
+        return self.data["type"] if "type" in self.data["type"] else None
+            
     @type.setter
     def type(self, type):
         self.data["type"] = type
